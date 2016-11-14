5f297069a912ff961db7c28b661d7f553844de53
Clacson <cookiellacson@gmail.com>
2016-11-14 13:20:26 +0000

    constructors mapped

--- app/src/main/java/com/example/user/martychart/MCApp.java
+++ app/src/main/java/com/example/user/martychart/MCApp.java
@@ -14,15 +14,28 @@ public class MCApp extends AppCompatActivity {
 
     DBController mController = new DBController(this);
     Button mAdd, mView, mUpdate, mDelete;
-    EditText mMedId, mMedicine, mQuantity, mTime;
-    TextView mInfoText;
+    EditText mMedId, mMedicine, mQuantity, mDate;
+    TextView mResultText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        
+        mMedId = (EditText) findViewById(R.id.etmedid);
+        mMedicine = (EditText) findViewById(R.id.etmed);
+        mQuantity = (EditText) findViewById(R.id.etquantity);
+        mDate = (EditText) findViewById(R.id.etdate);
+
+        mAdd = (Button) findViewById(R.id.btnadd);
+        mView = (Button) findViewById(R.id.btnview);
+        mUpdate = (Button) findViewById(R.id.btnupdate);
+        mDelete = (Button) findViewById(R.id.btndelete);
+
+        mResultText = (TextView) findViewById(R.id.txtresulttext);
+
+
+
     }
 
 }
