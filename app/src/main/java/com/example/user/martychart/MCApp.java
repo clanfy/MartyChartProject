2b1c81d07c16a652af44a6f78e1bb52b6f6d4773
Clacson <cookiellacson@gmail.com>
2016-11-14 13:15:25 +0000

    MCApp constructors created, onCreate method started

--- app/src/main/java/com/example/user/martychart/MCApp.java
+++ app/src/main/java/com/example/user/martychart/MCApp.java
@@ -1,10 +1,28 @@
 package com.example.user.martychart;
 
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 /**
  * Created by user on 14/11/2016.
  */
 
 public class MCApp extends AppCompatActivity {
+
+    DBController mController = new DBController(this);
+    Button mAdd, mView, mUpdate, mDelete;
+    EditText mMedId, mMedicine, mQuantity, mTime;
+    TextView mInfoText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        
+    }
+
 }
