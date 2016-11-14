da8f2382c9891eddf70e781afa88ae53520d3d03
Clacson <cookiellacson@gmail.com>
2016-11-14 12:00:29 +0000

    getAllMeds method completed, needs review

--- app/src/main/java/com/example/user/martychart/DBController.java
+++ app/src/main/java/com/example/user/martychart/DBController.java
@@ -1,8 +1,12 @@
 package com.example.user.martychart;
 
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 /**
  * Created by user on 14/11/2016.
@@ -14,7 +18,7 @@ public class DBController extends SQLiteOpenHelper {
     private static final String id = "ID"; //auto generated ID column
     private static final String medication = "medication"; //column name
     private static final String quantity = "quantity"; //column name
-    private static final String datetime = "date time"; //column name
+    private static final String time = "time"; //column name
     private static final String databasename = "medicationinfo"; //database name
     private static final int versioncode = 1; //version code of the database
 
@@ -28,7 +32,7 @@ public class DBController extends SQLiteOpenHelper {
         String query;
         query = "CREATE TABLE IF NOT EXISTS " + tablename +
                 "(" + id + "integer primary key, " + medication +
-                " text," + quantity + " text," + datetime + " text)";
+                " text," + quantity + " text," + time + " text)";
         database.execSQL(query);
     }
 
@@ -40,6 +44,27 @@ public class DBController extends SQLiteOpenHelper {
         onCreate(database);
     }
 
+    public ArrayList<HashMap<String, String>> getAllMeds() {
+        ArrayList<HashMap<String, String>> wordList;
+        wordList = new ArrayList<HashMap<String, String>>();
+        String selectQuery = "SELECT  * FROM " + tablename;
+        SQLiteDatabase database = this.getWritableDatabase();
+        Cursor cursor = database.rawQuery(selectQuery, null);
+        if (cursor.moveToFirst()) {
+            do {
+
+                HashMap<String, String> map = new HashMap<String, String>();
+                map.put("id", cursor.getString(0));
+                map.put("medication", cursor.getString(1));
+                map.put("quantity", cursor.getString(2));
+                map.put("time", cursor.getString(3));
+
+                wordList.add(map);
+            } while (cursor.moveToNext());
+        }
+
+        return wordList;
+    }
 
 
 
