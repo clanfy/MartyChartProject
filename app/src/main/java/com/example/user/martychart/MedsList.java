46c5cea30c338d34b78bffebf2909d0716c8fad5
Clacson <cookiellacson@gmail.com>
2016-11-14 12:17:08 +0000

    MedsList.java started

---
+++ app/src/main/java/com/example/user/martychart/MedsList.java
@@ -0,0 +1,18 @@
+package com.example.user.martychart;
+
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
+import android.widget.TextView;
+
+/**
+ * Created by user on 14/11/2016.
+ */
+
+public class MedsList extends AppCompatActivity {
+
+    DBController mcontroller = new DBController(this);
+    ListView mListView;
+    TextView mInfoText;
+    }
+
+}
