c63c1a69633c0018a7bbe53d50cf39fade84f769
Clacson <cookiellacson@gmail.com>
2016-11-14 11:53:15 +0000

    onCreate and onUpgrade methods completed

--- app/src/main/java/com/example/user/martychart/DBController.java
+++ app/src/main/java/com/example/user/martychart/DBController.java
@@ -17,19 +17,27 @@ public class DBController extends SQLiteOpenHelper {
     private static final String datetime = "date time"; //column name
     private static final String databasename = "medicationinfo"; //database name
     private static final int versioncode = 1; //version code of the database
-    //will also need quantity and time taken
+
 
     public DBController(Context context) {
         super(context, databasename, null, versioncode);
     }
 
     @Override
-    public void OnCreate(SQLiteDatabase database){
+    public void onCreate(SQLiteDatabase database){
         String query;
         query = "CREATE TABLE IF NOT EXISTS " + tablename +
                 "(" + id + "integer primary key, " + medication +
                 " text," + quantity + " text," + datetime + " text)";
         database.execSQL(query);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase database, int version_old, int current_version){
+        String query;
+        query = "DROP TABLE IF EXISTS " + tablename;
+        database.execSQL(query);
+        onCreate(database);
     }
 
 
