656a7af209ace0be9f6ea700993fb657ca36fc1a
Clacson <cookiellacson@gmail.com>
2016-11-15 20:34:02 +0000

    first test passed, medicationhasname

--- app/src/test/java/com/example/user/martychart/MedicationTest.java
+++ app/src/test/java/com/example/user/martychart/MedicationTest.java
@@ -1,5 +1,10 @@
 package com.example.user.martychart;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.Assert.assertEquals;
+
 /**
  * Created by user on 15/11/2016.
  */
@@ -7,5 +12,15 @@ package com.example.user.martychart;
 public class MedicationTest {
 
     Medication medication;
+
+    @Before
+    public void before(){
+        medication = new Medication (1, "Viagra", 2, 111216);
+    }
+
+    @Test
+    public void testMedicationHasName(){
+        assertEquals("Viagra", medication.getName());
+    }
 
 }
